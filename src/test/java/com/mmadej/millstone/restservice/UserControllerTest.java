d2a83ca453280d30c45cc533107d56698d1d63f4
Mark Madej <mark.madej@workiva.com>
2020-05-10 10:22:17 -0600

    Adding unit test

--- src/test/java/com/mmadej/millstone/restservice/UserControllerTest.java
+++ src/test/java/com/mmadej/millstone/restservice/UserControllerTest.java
@@ -1,13 +1,17 @@
 package com.mmadej.millstone.restservice;
 
+import com.mmadej.millstone.MillstoneApplication;
 import com.mmadej.millstone.domain.User;
 import com.mmadej.millstone.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
@@ -15,6 +19,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = MillstoneApplication.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
     @LocalServerPort
@@ -27,25 +33,26 @@ public class UserControllerTest {
     private UserController userController;
 
     private UserService mockUserService;
+    private User expected;
 
     @Before
     public void setup(){
         mockUserService = mock(UserService.class);
+
+        // Add a user (id=1) to the mock DB.
+        expected = new User(1, "Mark", "Madej");
+        when(mockUserService.getUser(1)).thenReturn(expected);
     }
 
     @Test
     public void getUserReturnsUser() throws Exception {
-        User expected = new User(1, "Mark", "Madej");
-
-        when(mockUserService.getUser(1)).thenReturn(expected);
-
-        // User returned here is null for some reason, need to figure that out
-        User actual = restTemplate.getForObject("http://localhost:" + port + "/user?id=1",
+        User actual = restTemplate.withBasicAuth("user", "password").getForObject("http://localhost:" + port + "/user?id=1",
                 User.class);
-
+        // Our test user (configured in setup) should be returned.
         assertThat(expected == actual);
     }
 
+    // TODO : Other methods to test
 //    public User getUser(final long id);
 //    public List<User> getUsers();
 //    public void createUser(final User user);
